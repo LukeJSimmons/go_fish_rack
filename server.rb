2c46ffbb52d26002dcb30e6a43686ffd0606d3df
Luke Simmons <lucasjsimmons@yahoo.com>
2025-06-23 10:22:20 -0400

    Add basic file structure

---
+++ server.rb
@@ -0,0 +1,32 @@
+require 'sinatra'
+require 'sinatra/json'
+require 'sinatra/respond_with'
+require 'rack/contrib'
+require_relative 'lib/game'
+require_relative 'lib/player'
+
+class Server < Sinatra::Base
+  enable :sessions
+  register Sinatra::RespondWith
+  use Rack::JSONBodyParser
+
+  def self.game
+    @@game ||= Game.new
+  end
+
+  get '/' do
+    slim :index
+  end
+
+  post '/join' do
+    player = Player.new(params['name'])
+    session[:current_player] = player
+    self.class.game.add_player(player)
+    redirect '/game'
+  end
+  
+  get '/game' do
+    redirect '/' if self.class.game.empty?
+    slim :game, locals: { game: self.class.game, current_player: session[:current_player] }
+  end
+end
